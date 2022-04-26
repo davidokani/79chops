236a15ddc38a0d5becc89213eca59f865bfdb873
JumokeN <93919148+JumokeN@users.noreply.github.com>
2022-04-26 23:00:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "./about/about.css";
+import App from "./about/About.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
