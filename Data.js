63eb1b6458eea06f7109b51bb2cfc530ed72915c
Seundave <103960289+Seundave@users.noreply.github.com>
2022-04-27 22:17:40 +0100

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,118 @@
+import product1 from './images/pizza-g14f461e7c_1920.jpg'
+import product2 from './images/pizza-g46b2f39a0_1920.jpg'
+import product3 from './images/pizza-g37346b7c3_1920.jpg'
+import product4 from './images/plate-2802332_1920.jpg'
+
+ const menu = [
+    {
+        id: 1,
+        img: product1,
+        alt: 'pizza',
+        category: 'Breakfast',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$29.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 2,
+        img: product2,
+        alt: 'pizza',
+        category: 'Lunch',
+        name: 'Hawania pizza',
+        desc: 'Marinara sauce, basil, italian sausage,roma tomatoes, olives and peston',
+        price: '$16.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 3,
+        img: product3,
+        alt: 'pizza',
+        category: 'Dinner',
+        name: 'Vegie Overload',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$14.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 4,
+        img: product4,
+        alt: 'pizza',
+        category: 'Breakfast',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$10.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 5,
+        img: product4,
+        alt: 'pizza',
+        category: 'Lunch',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$9.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 6,
+        img: product4,
+        alt: 'pizza',
+        category: 'Dinner',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$11.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 7,
+        img: product4,
+        alt: 'pizza',
+        category: 'Breakfast',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$15.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 8,
+        img: product4,
+        alt: 'pizza',
+        category: 'Lunch',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$7.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 9,
+        img: product4,
+        alt: 'pizza',
+        category: 'Dinner',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$9.9',
+        button: 'Add to cart',
+    },
+
+    {
+        id: 10,
+        img: product4,
+        alt: 'pizza',
+        category: 'Breakfast',
+        name: 'Supreme pizza',
+        desc: 'Marinara Sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$9.9',
+        button: 'Add to cart',
+    },
+]
+
+export default menu
