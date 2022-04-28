e4b2cb375f1d4839c78c32461d9dd6b9c14b944c
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-28 15:38:19 +0100

    navbar clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
+import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div>
-      <div className='logo'>79Chops</div>
       <Navbar />
       <div></div>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import "./about/about.css";
-import App from "./about/About.js";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
