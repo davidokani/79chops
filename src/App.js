18c830a7d4a0f14f50fe37046f60d4479e155b67
David Okani <itsdavidokani@gmail.com>
2022-04-28 04:02:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
       <div className='logo'>79Chops</div>
+      <Navbar />
       <div></div>
     </div>
   )
