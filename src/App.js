07bc2dacf08a7b5d0ea5c1cf5d02cb1776ae8b8f
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-30 05:42:21 +0100

    about page modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Home from './pages/home/Home'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/footer/Footer'
 import Footer from './components/footer/Footer'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Contact />
       <Footer />
     </div>
   )
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import "./about.css";
-import image1 from "./images/pic1.jpg";
-import image2 from "./images/pic2.jpg";
-import image3 from "./images/pic3.jpg";
-import image4 from "./images/pic4.jpg";
-import image5 from "./images/pic5.jpg";
+import React from 'react'
+import './about.css'
+// import image1 from '/public/images/pic1.jpg'
+// import image2 from '/public/images/pic2.jpg'
+// import image3 from '../../../public/images/pic3.jpg'
+// import image4 from '/public/images/pic4.jpg'
+// import image5 from '/images/pic5.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
+    <div className='App'>
+      <div className='content'>
         <h1>About Us</h1>
-        <div className="grid-flex">
-          <div className="col-image">
-            <img src={image1} alt="img1" />
+        <div className='grid-flex'>
+          <div className='col-image'>
+            <img src='/images/pic1.jpg' alt='img1' />
           </div>
-          <div className="col-text">
+          <div className='col-text'>
             <p>
               79Chops is a proudly African company that works in the hospitality
               industry in the region.
@@ -23,43 +23,43 @@ function App() {
           </div>
         </div>
 
-        <div className="grid-flex">
-          <div className="col-image">
-            <img src={image2} alt="img2" />
+        <div className='grid-flex'>
+          <div className='col-image'>
+            <img src='/images/pic2.jpg' alt='img2' />
           </div>
-          <div className="col-text">
+          <div className='col-text'>
             <p>The brand operates through several stores in Africa.</p>
           </div>
         </div>
 
-        <div className="grid-flex">
-          <div className="col-image">
-            <img src={image3} alt="img3" />
+        <div className='grid-flex'>
+          <div className='col-image'>
+            <img src='/images/pic3.jpg' alt='img3' />
           </div>
-          <div className="col-text">
+          <div className='col-text'>
             <p>
-              We're your go to place for fine dining and unique experiences{" "}
+              We're your go to place for fine dining and unique experiences{' '}
             </p>
           </div>
         </div>
 
-        <div className="grid-flex">
-          <div className="col-image">
-            <img src={image4} alt="img4" />
+        <div className='grid-flex'>
+          <div className='col-image'>
+            <img src='/images/pic4.jpg' alt='img4' />
           </div>
-          <div className="col-text">
+          <div className='col-text'>
             <p>
               Our mission is to be the most sought-after experience in
-              indiginous cuisine.{" "}
+              indiginous cuisine.{' '}
             </p>
           </div>
         </div>
 
-        <div className="grid-flex">
-          <div className="col-image">
-            <img src={image5} alt="img5" />
+        <div className='grid-flex'>
+          <div className='col-image'>
+            <img src='/images/pic5.jpg' alt='img5' />
           </div>
-          <div className="col-text">
+          <div className='col-text'>
             <p>
               Our fundamental principles are INNOVATION, QUALITY, UNPARALLELED
               AMBIENCE, and CONSISTENCY.
@@ -68,7 +68,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
