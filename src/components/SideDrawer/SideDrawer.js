9f7c72a3b50d9f79ff7e3d580dacd9be04b2d58d
David Okani <itsdavidokani@gmail.com>
2022-04-30 20:28:50 +0100

    nav linking done at 80%

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import './SideDrawer.css'
 
@@ -11,16 +12,16 @@ const sideDrawer = (props) => {
     <nav className={drawerClasses}>
       <ul>
         <li>
-          <a href='/'>Home</a>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <a href='/'>Product</a>
+          <Link to='/about'>About</Link>
         </li>
         <li>
-          <a href='/'>Contact</a>
+          <Link to='/contact'>Contact</Link>
         </li>
         <li>
-          <a href='/'>About</a>
+          <Link to='/products'>Products</Link>
         </li>
       </ul>
     </nav>
