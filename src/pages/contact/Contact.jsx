e7c05b312c4ea5e3281f61c61597dc1b8b786326
David Okani <itsdavidokani@gmail.com>
2022-05-01 01:34:13 +0100

    contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,7 +4,7 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div3>
+    <div>
       <h1>Contact page</h1>
       <div className='contact-pic'>
         <img src='/images/pic2.jpg' alt='img2' />
@@ -17,7 +17,40 @@ const Contact = () => {
       </p>
       <p>To contact or reach out to us,please see the details below: </p>
       <h3>79chops contact form</h3>
-      <fieldset>
+      <div class="content-input">
+
+        <div class="container">
+
+          <form action="">
+
+            <label for="name">Name</label><br />
+
+            <input type="text" placeholder="Enter Name" class="contact__input" required /><br />
+
+            <label for="email">Email</label><br />
+
+            <input type="mail" placeholder="Enter Email" class="contact__input" required /><br />
+
+            <label for="message">Messeage</label><br />
+
+            <textarea name="" id="" cols="25" rows="10" class="contact__input" required></textarea>
+
+            <br />
+
+            <input type="submit" value="Send" class="contact_input button" />
+
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+};
+export default Contact;
+
+/* <fieldset>
         <label for='name'>Name: </label>
         <br />
         <input
@@ -87,17 +120,17 @@ const Contact = () => {
 
         <br />
         <button type='submit'>Subscribe</button>
-      </fieldset>
+      </fieldset> */
 
-      {/* <h3>Stay in Touch!</h3>
+/* <h3>Stay in Touch!</h3>
       <a href='#'>Instagram</a>
       <br />
       <a href='#'>Twitter</a>
       <br />
       <a href='#'>Linkedln</a>
-      <br /> */}
-    </div3>
-  )
+      <br /> 
+                  </div>
+                    )
 }
 
-export default Contact
+              export default Contact */
